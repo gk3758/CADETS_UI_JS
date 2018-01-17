cf14575920f7d3da3244c6aa8e7012775ee7b956
Unknown <gk3758@mun.ca>
2018-01-17 17:11:47 -0330

    workSheetButtons function

--- js/bundle.js
+++ js/bundle.js
@@ -67,124 +67,133 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-var worksheetGraph = cytoscape({
+var testGraph = cytoscape({
 	container: document.getElementById('worksheetGraph'),
-	elements: [
-	{ data: { id: 'EN-1020', name: 'Introduction to Programming' } },
-	{ data: { id: 'EN-1040', name: 'Mechanisms and Electric Circuits' } },
-	{ data: { id: 'EN-3861', name: 'Digital Logic' } },
-	{ data: { id: 'EN-3891', name: 'Foundations of Programming' } },
-	{ data: { id: 'EN-4862', name: 'Microprocessors' } },
-	{ data: { id: 'EN-4892', name: 'Data Structures' } },
-	{ data: { id: 'EN-5865', name: 'Digital Systems' } },
-	{ data: { id: 'EN-5895', name: 'Software Design' } },
-	{ data: { id: 'EN-6861', name: 'Computer Architecture' } },
-	{ data: { id: 'EN-6892', name: 'Algorithms: Complexity and Correctness' } },
-	{ data: { id: 'EN-7894', name: 'Concurrent Programming' } },
-	{ data: { id: 'EN-8894', name: 'Real-time Operating Systems' } },
-	{
-		data: {
-			id: '1020-3891',
-			source: 'EN-1020',
-			target: 'EN-3891'
-		}
-	},
-	{
-		data: {
-			id: '1040-3861',
-			source: 'EN-1040',
-			target: 'EN-3861'
-		}
-	},
-	{
-		data: {
-			id: '3861-4862',
-			source: 'EN-3861',
-			target: 'EN-4862'
-		}
-	},
-	{
-		data: {
-			id: '3891-4892',
-			source: 'EN-3891',
-			target: 'EN-4892'
-		}
-	},
-	{
-		data: {
-			id: '3891-5865',
-			source: 'EN-3891',
-			target: 'EN-5865'
-		}
-	},
-	{
-		data: {
-			id: '4862-5865',
-			source: 'EN-4862',
-			target: 'EN-5865'
-		}
-	},
-	{
-		data: {
-			id: '4892-5895',
-			source: 'EN-4892',
-			target: 'EN-5895'
-		}
-	},
-	{
-		data: {
-			id: '5865-6861',
-			source: 'EN-5865',
-			target: 'EN-6861'
-		}
-	},
-	{
-		data: {
-			id: '5895-6892',
-			source: 'EN-5895',
-			target: 'EN-6892'
-		}
-	},
-	{
-		data: {
-			id: '6861-7894',
-			source: 'EN-6861',
-			target: 'EN-7894'
-		}
-	},
-	{
-		data: {
-			id: '6892-7894',
-			source: 'EN-6892',
-			target: 'EN-7894'
-		}
-	},
-	{
-		data: {
-			id: '7894-8894',
-			source: 'EN-7894',
-			target: 'EN-8894'
-		}
-	}],
-	style: cytoscape.stylesheet()
-	.selector('node')
-	.css({
-		'content': 'data(id)',
-		'text-valign': 'center',
-		'color': 'white',
-		'text-outline-width': 2,
-		'background-color': 'red',
-		'text-outline-color': 'black'
-	})
-	.selector('edge')
-	.css({
-		'curve-style': 'bezier',
-		'target-arrow-shape': 'triangle',
-		'target-arrow-color': 'black',
-		'line-color': 'gray',
-		'width': 1
-	})
+	boxSelectionEnabled: true,
 });
+
+var worksheetGraph = {
+  graph: testGraph
+ };
+
+// var cy = cytoscape({
+// 	container: document.getElementById('worksheetGraph'),
+// 	elements: [
+// 	{ data: { id: 'EN-1020', name: 'Introduction to Programming' } },
+// 	{ data: { id: 'EN-1040', name: 'Mechanisms and Electric Circuits' } },
+// 	{ data: { id: 'EN-3861', name: 'Digital Logic' } },
+// 	{ data: { id: 'EN-3891', name: 'Foundations of Programming' } },
+// 	{ data: { id: 'EN-4862', name: 'Microprocessors' } },
+// 	{ data: { id: 'EN-4892', name: 'Data Structures' } },
+// 	{ data: { id: 'EN-5865', name: 'Digital Systems' } },
+// 	{ data: { id: 'EN-5895', name: 'Software Design' } },
+// 	{ data: { id: 'EN-6861', name: 'Computer Architecture' } },
+// 	{ data: { id: 'EN-6892', name: 'Algorithms: Complexity and Correctness' } },
+// 	{ data: { id: 'EN-7894', name: 'Concurrent Programming' } },
+// 	{ data: { id: 'EN-8894', name: 'Real-time Operating Systems' } },
+// 	{
+// 		data: {
+// 			id: '1020-3891',
+// 			source: 'EN-1020',
+// 			target: 'EN-3891'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '1040-3861',
+// 			source: 'EN-1040',
+// 			target: 'EN-3861'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '3861-4862',
+// 			source: 'EN-3861',
+// 			target: 'EN-4862'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '3891-4892',
+// 			source: 'EN-3891',
+// 			target: 'EN-4892'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '3891-5865',
+// 			source: 'EN-3891',
+// 			target: 'EN-5865'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '4862-5865',
+// 			source: 'EN-4862',
+// 			target: 'EN-5865'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '4892-5895',
+// 			source: 'EN-4892',
+// 			target: 'EN-5895'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '5865-6861',
+// 			source: 'EN-5865',
+// 			target: 'EN-6861'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '5895-6892',
+// 			source: 'EN-5895',
+// 			target: 'EN-6892'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '6861-7894',
+// 			source: 'EN-6861',
+// 			target: 'EN-7894'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '6892-7894',
+// 			source: 'EN-6892',
+// 			target: 'EN-7894'
+// 		}
+// 	},
+// 	{
+// 		data: {
+// 			id: '7894-8894',
+// 			source: 'EN-7894',
+// 			target: 'EN-8894'
+// 		}
+// 	}],
+// 	style: cytoscape.stylesheet()
+// 	.selector('node')
+// 	.css({
+// 		'content': 'data(id)',
+// 		'text-valign': 'center',
+// 		'color': 'white',
+// 		'text-outline-width': 2,
+// 		'background-color': 'red',
+// 		'text-outline-color': 'black'
+// 	})
+// 	.selector('edge')
+// 	.css({
+// 		'curve-style': 'bezier',
+// 		'target-arrow-shape': 'triangle',
+// 		'target-arrow-color': 'black',
+// 		'line-color': 'gray',
+// 		'width': 1
+// 	})
+// });
 
 var cy2 = cytoscape({
 	container: document.getElementById('inspectorGraph'),
@@ -305,9 +314,6 @@ var cy2 = cytoscape({
 	})
 });
 
-//var string = cy.json();
-//console.log(string);
-
 //Button logic
 document.getElementById("hideAnalysisWorksheet").onclick = function () { 
   $('#analysisWorksheet').toggleClass('collapsedAnalysisWorksheet');
@@ -315,11 +321,22 @@ document.getElementById("hideAnalysisWorksheet").onclick = function () {
   $('#worksheetGraph').toggleClass('expandedWorksheet');
 };
 
+document.getElementById("loadGraph").onchange = function () {
+	load(this.files[0], worksheetGraph);
+};
+
+document.getElementById("saveGraph").onclick = function () { 
+	save(worksheetGraph.graph);
+};
+
+//layout from graphing.js
+document.getElementById("reDagre").onclick = function () { 
+	layout( worksheetGraph.graph, 'cose'); //TODO: get cDagre online
+};
+
+//layout from graphing.js
 document.getElementById("reCose-Bilkent").onclick = function () { 
-	var layout = worksheetGraph.layout({
-	  name: 'cose'
-	});
-	layout.run();
+	layout( worksheetGraph.graph, 'cose'); //TODO: get cose-bilkent online
 };
 
 /***/ })
